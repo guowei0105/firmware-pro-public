ab4fb77c93e184bec58fbad109fd518384c4f34f
guowei0105 <155709160+guowei0105@users.noreply.github.com>
2024-04-16 18:19:50 +0800

    fix: adjust version display style (#107)
    adjush version display style

--- core/src/trezor/lvglui/scrs/deviceinfo.py
+++ core/src/trezor/lvglui/scrs/deviceinfo.py
@@ -8,13 +8,19 @@ class DeviceInfoManager:
     _instance = None
     preloaded_info = {}
 
+    def format_version(self, version, build_id, short_hash):
+        if build_id and "unknown" not in build_id:
+            return f"{version} [{build_id}-{short_hash}]"
+        else:
+            return f"{version} [{short_hash}]"
+
     def preload_device_info(self):
         version = device.get_firmware_version()
         build_id = utils.BUILD_ID[-7:].decode()
         firmware_hash = utils.onekey_firmware_hash()
         hex_hash = binascii.hexlify(firmware_hash).decode("ascii")
         short_hash = hex_hash[:7]
-        version = f"{version} [{build_id}-{short_hash}]"
+        version = self.format_version(version, build_id, short_hash)
 
         ble_name = device.get_ble_name() or uart.get_ble_name()
         ble_version = uart.get_ble_version()
@@ -22,22 +28,22 @@ class DeviceInfoManager:
         ble_hash = uart.get_ble_hash()
         ble_hex_hash = binascii.hexlify(ble_hash).decode("ascii")
         ble_short_hash = ble_hex_hash[:7]
-        ble_version = f"{ble_version} [{ble_build}-{ble_short_hash}]"
+        ble_version = self.format_version(ble_version, ble_build, ble_short_hash)
 
         boot_version = utils.boot_version()
         boot_hash = utils.boot_hash()
         boot_hex_hash = binascii.hexlify(boot_hash).decode("ascii")
         boot_short_hash = boot_hex_hash[:7]
         boot_build_id = utils.boot_build_id()
-        boot_version = f"{boot_version} [{boot_build_id}-{boot_short_hash}]"
+        boot_version = self.format_version(boot_version, boot_build_id, boot_short_hash)
 
         onekey_se01_version = device.get_se01_version()
         onekey_se01_hash = device.get_se01_hash()
         onekey_se01_hex_hash = binascii.hexlify(onekey_se01_hash).decode("ascii")
         onekey_se01_short_hash = onekey_se01_hex_hash[:7]
         onekey_se01_build_id = device.get_se01_build_id()
-        onekey_se01_version = (
-            f"{onekey_se01_version} [{onekey_se01_build_id}-{onekey_se01_short_hash}]"
+        onekey_se01_version = self.format_version(
+            onekey_se01_version, onekey_se01_build_id, onekey_se01_short_hash
         )
 
         onekey_se02_version = device.get_se02_version()
@@ -45,8 +51,8 @@ class DeviceInfoManager:
         onekey_se02_hex_hash = binascii.hexlify(onekey_se02_hash).decode("ascii")
         onekey_se02_short_hash = onekey_se02_hex_hash[:7]
         onekey_se02_build_id = device.get_se02_build_id()
-        onekey_se02_version = (
-            f"{onekey_se02_version} [{onekey_se02_build_id}-{onekey_se02_short_hash}]"
+        onekey_se02_version = self.format_version(
+            onekey_se02_version, onekey_se02_build_id, onekey_se02_short_hash
         )
 
         onekey_se03_version = device.get_se03_version()
@@ -54,8 +60,8 @@ class DeviceInfoManager:
         onekey_se03_hex_hash = binascii.hexlify(onekey_se03_hash).decode("ascii")
         onekey_se03_short_hash = onekey_se03_hex_hash[:7]
         onekey_se03_build_id = device.get_se03_build_id()
-        onekey_se03_version = (
-            f"{onekey_se03_version} [{onekey_se03_build_id}-{onekey_se03_short_hash}]"
+        onekey_se03_version = self.format_version(
+            onekey_se03_version, onekey_se03_build_id, onekey_se03_short_hash
         )
 
         onekey_se04_version = device.get_se04_version()
@@ -63,8 +69,8 @@ class DeviceInfoManager:
         onekey_se04_hex_hash = binascii.hexlify(onekey_se04_hash).decode("ascii")
         onekey_se04_short_hash = onekey_se04_hex_hash[:7]
         onekey_se04_build_id = device.get_se04_build_id()
-        onekey_se04_version = (
-            f"{onekey_se04_version} [{onekey_se04_build_id}-{onekey_se04_short_hash}]"
+        onekey_se04_version = self.format_version(
+            onekey_se04_version, onekey_se04_build_id, onekey_se04_short_hash
         )
 
         onekey_se01_boot_version = device.get_se01_boot_version()
@@ -74,7 +80,11 @@ class DeviceInfoManager:
         )
         onekey_se01_boot_short_hash = onekey_se01_boot_hex_hash[:7]
         onekey_se01_boot_build_id = device.get_se01_boot_build_id()
-        onekey_se01_boot_version = f"{onekey_se01_boot_version} [{onekey_se01_boot_build_id}-{onekey_se01_boot_short_hash}]"
+        onekey_se01_boot_version = self.format_version(
+            onekey_se01_boot_version,
+            onekey_se01_boot_build_id,
+            onekey_se01_boot_short_hash,
+        )
 
         onekey_se02_boot_version = device.get_se02_boot_version()
         onekey_se02_boot_hash = device.get_se02_boot_hash()
@@ -83,7 +93,11 @@ class DeviceInfoManager:
         )
         onekey_se02_boot_short_hash = onekey_se02_boot_hex_hash[:7]
         onekey_se02_boot_build_id = device.get_se02_boot_build_id()
-        onekey_se02_boot_version = f"{onekey_se02_boot_version} [{onekey_se02_boot_build_id}-{onekey_se02_boot_short_hash}]"
+        onekey_se02_boot_version = self.format_version(
+            onekey_se02_boot_version,
+            onekey_se02_boot_build_id,
+            onekey_se02_boot_short_hash,
+        )
 
         onekey_se03_boot_version = device.get_se03_boot_version()
         onekey_se03_boot_hash = device.get_se03_boot_hash()
@@ -92,7 +106,11 @@ class DeviceInfoManager:
         )
         onekey_se03_boot_short_hash = onekey_se03_boot_hex_hash[:7]
         onekey_se03_boot_build_id = device.get_se03_boot_build_id()
-        onekey_se03_boot_version = f"{onekey_se03_boot_version} [{onekey_se03_boot_build_id}-{onekey_se03_boot_short_hash}]"
+        onekey_se03_boot_version = self.format_version(
+            onekey_se03_boot_version,
+            onekey_se03_boot_build_id,
+            onekey_se03_boot_short_hash,
+        )
 
         onekey_se04_boot_version = device.get_se04_boot_version()
         onekey_se04_boot_hash = device.get_se04_boot_hash()
@@ -101,7 +119,11 @@ class DeviceInfoManager:
         )
         onekey_se04_boot_short_hash = onekey_se04_boot_hex_hash[:7]
         onekey_se04_boot_build_id = device.get_se04_boot_build_id()
-        onekey_se04_boot_version = f"{onekey_se04_boot_version} [{onekey_se04_boot_build_id}-{onekey_se04_boot_short_hash}]"
+        onekey_se04_boot_version = self.format_version(
+            onekey_se04_boot_version,
+            onekey_se04_boot_build_id,
+            onekey_se04_boot_short_hash,
+        )
         serial = device.get_serial()
         model = device.get_model()
         board_version = utils.board_version()
