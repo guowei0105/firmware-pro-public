6decb51101f8350b36858008adc1c3280c658dc7
Larrie/Leon <larriesun2007@gmail.com>
2025-04-17 11:32:45 +0800

    fix(lvgl_tick): correct lvgl timer configuration. (#295)

--- core/embed/trezorhal/timer.c
+++ core/embed/trezorhal/timer.c
@@ -2,15 +2,19 @@
 
 #include "lvgl.h"
 
+#define LVGL_TIMER_PERIOD_MS 2
+
+#define PRESCALE_VALUE 1000
+
 static TIM_HandleTypeDef TimHandle;
 static void lvgl_timer_init(void) {
   __HAL_RCC_TIM4_CLK_ENABLE();
 
   TimHandle.Instance = TIM4;
 
-  // 1 ms
-  TimHandle.Init.Period = 100 - 1;
-  TimHandle.Init.Prescaler = (uint32_t)(SystemCoreClock / (2 * 1000)) - 1;
+  TimHandle.Init.Period =
+      SystemCoreClock / PRESCALE_VALUE / (2 * 1000) * LVGL_TIMER_PERIOD_MS - 1;
+  TimHandle.Init.Prescaler = PRESCALE_VALUE - 1;
   TimHandle.Init.ClockDivision = 0;
   TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
   TimHandle.Init.RepetitionCounter = 0;
@@ -30,6 +34,11 @@ static void lvgl_timer_init(void) {
 
 void timer_init(void) { lvgl_timer_init(); }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) { lv_tick_inc(1); }
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+  if (htim->Instance == TIM4) {
+    // Call lv_tick_inc() every LVGL_TIMER_PERIOD_MS ms
+    lv_tick_inc(LVGL_TIMER_PERIOD_MS);
+  }
+}
 
 void TIM4_IRQHandler(void) { HAL_TIM_IRQHandler(&TimHandle); }
