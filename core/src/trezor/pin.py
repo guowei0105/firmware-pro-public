c00a3097cb76f426b07f19237a83bb643ce81fca
lihuanhuan <lihuanhuan@users.noreply.github.com>
2025-07-16 12:26:16 +0800

    feat(ui): draw progress bar using LVGL components. (#354)

--- core/src/trezor/pin.py
+++ core/src/trezor/pin.py
@@ -6,84 +6,87 @@ keepalive_callback: Any = None
 
 _pre_scr: Any = None
 _scr: Any = None
+_timer: Any = None
+
+
+def _timer_callback(timer: Any) -> None:
+
+    global _scr, _timer
+
+    if _scr is not None:
+        try:
+            _scr.delete()
+            _scr = None
+            if _timer is not None:
+                _timer._del()
+                _timer = None
+        except Exception:
+            _scr = None
+            _timer = None
 
 
 def show_pin_timeout(seconds: int, progress: int, message: str) -> bool:
-    from trezor import ui
     from trezor.lvglui.scrs import lv
     from trezor.lvglui.scrs.common import FullSizeWindow
+    from trezor.lvglui.i18n import gettext as _, keys as i18n_keys
+    from trezor.lvglui.lv_colors import lv_colors
 
     global _previous_progress
-    global _previous_seconds
-    global _scr
+    global _scr, _timer
 
     if callable(keepalive_callback):
         keepalive_callback()
 
-    if message is not None:
-        text = ""
-        if message == "read fp data":
+    _scr_need_create = _scr is None
+    if _scr_need_create:
+        try:
+            _scr.subtitle
+        except Exception:
+            _scr = None
+            _scr_need_create = True
+
+    if _scr_need_create:
+        if message is not None:
             text = ""
+            if message == "read fp data":
+                text = _(i18n_keys.MSG__READING_FINGERPRINT_FROM_SECURITY_CHIP)
+            else:
+                text = message
         else:
-            text = message
+            text = ""
 
-        if _scr is None:
-            _scr = FullSizeWindow(title="", subtitle=text)
-            _scr.subtitle.set_size(480, 200)
-            _scr.subtitle.set_pos(0, 462)
-            _scr.subtitle.set_style_text_align(lv.TEXT_ALIGN.CENTER, 0)
-            if progress != 0:
-                lv.refr_now(None)
+        _scr = FullSizeWindow(title="", subtitle=text)
+        _scr.subtitle.set_size(480, 200)
+        _scr.subtitle.set_pos(0, 462)
+        _scr.subtitle.set_style_text_align(lv.TEXT_ALIGN.CENTER, 0)
 
-    else:
-        if _scr is not None:
-            try:
-                _scr.delete()
-                _scr = None
-            except Exception:
-                _scr = None
+        arc_loader = lv.arc(_scr)
+        arc_loader.set_bg_angles(0, 360)
+        arc_loader.set_size(160, 160)
+        arc_loader.set_rotation(270)
+        arc_loader.set_angles(0, 0)
+        arc_loader.remove_style(None, lv.PART.KNOB)
+        arc_loader.set_style_arc_color(lv_colors.WHITE, lv.PART.INDICATOR)
+        arc_loader.center()
 
-    if progress == 0:
-        if progress != _previous_progress:
-            # avoid overdraw in case of repeated progress calls
-            ui.display.clear()
-            ui.display.loader(progress, False, 0, ui.FG, ui.BG)
-            _previous_seconds = None
-        # ui.display.text_center(ui.WIDTH // 2, 37, message, ui.BOLD, ui.FG, ui.BG)
-        if _scr is not None:
-            try:
-                _scr.invalidate()
-                lv.refr_now(None)
-            except Exception:
-                _scr = None
+        _scr.arc_loader = arc_loader
 
-    # if not utils.DISABLE_ANIMATION:
-    else:
-        ui.display.loader(progress, False, 0, ui.FG, ui.BG)
+        _timer = lv.timer_create(_timer_callback, 10, None)
+        _timer.set_repeat_count(1)
 
-    if seconds != _previous_seconds:
-        if seconds == 0:
-            # remaining = ""
-            ui.display.clear()
-        # elif seconds == 1:
-        #     remaining = "1 second left"
-        # else:
-        #     remaining = f"{seconds} seconds left"
-        ui.display.bar(0, ui.HEIGHT - 42, ui.WIDTH, 25, ui.BG)
-        # ui.display.text_center(
-        #     ui.WIDTH // 2, ui.HEIGHT - 22, remaining, ui.BOLD, ui.FG, ui.BG
-        # )
-        _previous_seconds = seconds
+    _scr.arc_loader.set_end_angle(progress * 360 // 1000)
+    lv.refr_now(None)
 
     if progress == 1000:
-        ui.display.clear()
         if _scr is not None:
             try:
                 _scr.delete()
                 _scr = None
+                if _timer is not None:
+                    _timer._del()
+                    _timer = None
             except Exception:
                 _scr = None
+                _timer = None
 
-    ui.refresh()
-    _previous_progress = progress
-    return False
+    return True
